0b11d4e0f98c3cc704e8a5e8d58f7bd6fda22c35
audreydk <audreydk@cs.washington.edu>
2024-04-09 13:43:47 -0700

    first 2 mathOp function test passes

--- ComplexCalc.playground/Contents.swift
+++ ComplexCalc.playground/Contents.swift
@@ -79,6 +79,9 @@ class Calculator {
         return add(nums) / nums.count;
     }
     
+    func mathOp(lhs: Int, rhs: Int, op: (Int, Int) -> Int) -> Int{
+        return op(lhs, rhs)
+    }
 }
 
 //: Don't change the name of this object (`calc`); it's used in all the tests.
@@ -103,7 +106,7 @@ calc.subtract(lhs: 2, rhs: 2) == 0
 calc.multiply(lhs: 2, rhs: 2) == 4
 calc.divide(lhs: 2, rhs: 2) == 1
 
-calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rjs) + (lhs * rhs) }) == 35
+calc.mathOp(lhs: 5, rhs: 5, op: { (lhs: Int, rhs: Int) -> Int in (lhs + rhs) + (lhs * rhs) }) == 35
     // This style is one way of writing an anonymous function
 calc.mathOp(lhs: 10, rhs: -5, op: { ($0 + $1) + ($0 - $1) }) == 20
     // This is the second, more terse, style; either works
